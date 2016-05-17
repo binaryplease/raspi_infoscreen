486aab76afa9934ad83838c05c82fa8c43ab5294
binaryplease <binaryplease@gmail.com>
2016-05-17 12:08:33 +0200

    keyscript navigates pages now

--- js/keynavigation.js
+++ js/keynavigation.js
@@ -8,7 +8,20 @@ function keyDownTextField(e) {
 		case 13:
 			alert("You hit the enter key.");
 			break;
-		case n:
+		case 49:
+			window.location.replace('page1.html');
+			break;
+		case 50:
+			window.location.replace('page2.html');
+			break;
+		case 51:
+			window.location.replace('page3.html');
+			break;
+		case 52:
+			window.location.replace('page4.html');
+			break;
+		case 53:
+			window.location.replace('page5.html');
 			break;
 		default:
 			alert(keyCode);
